b11d84aa5f8272e096daeb50a41ec0cf6ea8f15f
moeez71 <atlasmoeez@gmail.com>
2022-03-07 12:08:18 +0500

    2nd push

---
+++ src/main/java/uk/co/planetbeyond/service/responsebeans/NotificationData.java
@@ -0,0 +1,55 @@
+/*
+ * package uk.co.planetbeyond.service.responsebeans;
+ * 
+ * public class NotificationData {
+ * 
+ * public void test() {
+ * 
+ * 
+ * while (resultSet.next()) { NotificationResponseBean bean = new NotificationResponseBean();
+ * 
+ * bean.setNotificationId(resultSet.getInt("notification_id")); bean.setNotificationMessage(resultSet.getString("notification_message"));
+ * bean.setNotificationTitle(resultSet.getString("notification_title")); bean.setStatus(resultSet.getInt("status"));
+ * bean.setScheduledTimestamp(resultSet.getDate("broadcast_timestamp"); bean.setBroadcastTimestamp(resultSet.getDate("scheduled_timestamp");
+ * 
+ * 
+ * SavedCandidateResponseBean bean1 = new SavedCandidateResponseBean(); bean1.setCandidateMsisdn(resultSet.getString("candidate_msisdn"));
+ * bean1.setEmployerMsisdn(resultSet.getString("employer_msisdn"));
+ * bean1.setEmployerSavedCandidateId(resultSet.getInt("employer_saved_candidate_id")); bean1.setStatus(resultSet.getInt("status");
+ * 
+ * CandidateResponseBean bean2 = new CandidateResponseBean(); bean2.setCityId(resultSet.getInt("city_id"));
+ * bean2.setCnic(resultSet.getString("cnic")); bean2.setDeviceId(resultSet.getString("device_id"));
+ * bean2.setExperience(resultSet.getInt("experience")); bean2.setImageName(resultSet.getString("image_name"));
+ * bean2.setMsisdn(resultSet.getString("msisdn")); bean2.setName(resultSet.getString("name"));
+ * bean2.setProfileCreatedThrough(resultSet.getInt("profile_created_through")); bean2.setOtp(resultSet.getInt("otp"));
+ * bean2.setIsTelenorUser(resultSet.getInt("is_telenor_user"));
+ * 
+ * 
+ * EmployerResponseBean bean3 = EmployerResponseBean(); bean3.setAddress(resultSet.getString("image_name"));
+ * bean3.setCityId(resultSet.getInt("city_id")); bean3.setCnic(resultSet.getString("cnic")); bean3.setDeviceId(resultSet.getString("device_id"));
+ * bean3.setEmployerBase(resultSet.getString("device_id")); bean3.setExperience(resultSet.getInt("experience"));
+ * bean3.setImageName(resultSet.getString("image_name")); bean3.setMsisdn(resultSet.getString("msisdn")); bean3.setName(resultSet.getString("name"));
+ * bean3.setProfileCreatedThrough(resultSet.getInt("profile_created_through")); bean3.setType(resultSet.getInt("type"));
+ * bean3.setEmployerBase(resultSet.getString("employer_base")); bean3.setIsTelenorUser(resultSet.getInt("is_telenor_user"));
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * bean.setIsPostExpire(resultSet.getBoolean("is_post_expire"));
+ * 
+ * bean.setIsFeaturedJob(resultSet.getBoolean("is_featured_job")); bean.setExpirationDate(resultSet.getDate("expiration_date"));
+ * bean.setCreationTimestamp(resultSet.getDate("creation_timestamp")); bean.setMaxApplicants(resultSet.getInt("max_applicants"));
+ * bean.setDescription(resultSet.getString("description")); bean.setSalary(resultSet.getInt("salary"));
+ * bean.setCountry(resultSet.getString("country")); bean.setExperienceRequired(resultSet.getInt("experience_required"));
+ * bean.setCompanyName(resultSet.getString("company_name")); bean.setType(resultSet.getInt("type"));
+ * bean.setImageName(resultSet.getString("image_name")); bean.setTitle(resultSet.getString("title")); bean.setSkillId(resultSet.getInt("skill_id"));
+ * bean.setCityId(resultSet.getInt("city_id")); bean.setEmployerMsisdn(resultSet.getString("employer_msisdn"));
+ * bean.setJobId(resultSet.getInt("job_id"));
+ * 
+ * list.add(bean);
+ * 
+ * } } }
+ */
